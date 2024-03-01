1e1da0d6907d4181c6675553ab20bfe2051e9f5b
Bsrat <bsret-kidane@hotmail.no>
2024-03-01 16:36:31 +0100

    Added multiple files excluding non python files

---
+++ script1.py
@@ -0,0 +1,4 @@
+# script1.py
+
+def my_function():
+    print("This is a function from script1.py")
---
+++ script2.py
@@ -0,0 +1,10 @@
+# script2.py
+
+from script1 import my_function
+
+def main():
+    print("Running script2.py")
+    my_function()
+
+if __name__ == "__main__":
+    main()
